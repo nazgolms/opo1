a2fd3e3be83741bec3d23cf1b9447ca76627c366
nazgolms <nazgol76@hotmail.com>
2026-05-22 19:06:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function login() {
 function register() {
   window.location.href = "dashboard.html";
 }
+
+function login() {
+  const email = document.getElementById("nazgol76@gmail.com").value;
+  const password = document.getElementById("123456").value;
+
+  if (email && password) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("nazgol76@gmail.com 123456");
+  }
+}
