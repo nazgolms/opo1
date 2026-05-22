0585c215718c9839556e630156f1028791ac6ab8
nazgolms <nazgol76@hotmail.com>
2026-05-22 20:04:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function login(){
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-  if(email && password){
+    if(email !== "" && password !== ""){
 
-    document.getElementById("loginPage").style.display = "none";
+        document.getElementById("loginPage").style.display = "none";
 
-    document.getElementById("dashboard").classList.remove("hidden");
+        document.getElementById("dashboard").classList.remove("hidden");
 
-  } else {
+    }
 
-    alert("Please enter email and password");
+    else{
 
-  }
+        alert("Please enter email and password");
+
+    }
 
 }
