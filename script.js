1deecd0b34d366b44d8d74bafa2bd97d2603ea54
nazgolms <nazgol76@hotmail.com>
2026-05-22 21:48:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+<script>
+
 function login(){
 
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+document.getElementById("loginPage").style.display = "none";
 
-    if(email !== "" && password !== ""){
-
-        document.getElementById("loginPage").style.display = "none";
-
-        document.getElementById("dashboard").classList.remove("hidden");
-
-    }
-
-    else{
-
-        alert("Please enter email and password");
-
-    }
+document.getElementById("dashboard").style.display = "block";
 
 }
+
+function showPage(pageId){
+
+document.getElementById("dashboardPage").style.display = "none";
+document.getElementById("chatPage").style.display = "none";
+document.getElementById("ticketsPage").style.display = "none";
+document.getElementById("clientsPage").style.display = "none";
+document.getElementById("performancePage").style.display = "none";
+
+document.getElementById(pageId).style.display = "block";
+
+}
+
+</script>
